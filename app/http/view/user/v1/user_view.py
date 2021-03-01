2cb44a55aed55ae9aef70475f22dcfb4793fd5c1
simon shim <samnaka94@gmail.com>
2021-03-01 14:28:11 +0900

    [modify] user 업데이트는 fox에서 처리하므로 주석

--- app/http/view/user/v1/user_view.py
+++ app/http/view/user/v1/user_view.py
@@ -30,17 +30,18 @@ def get_user_view(user_id):
     return UserPresenter().transform(GetUserUseCase().execute(dto=dto))
 
 
-@api.route("/user/v1/users/<int:user_id>", methods=["PUT"])
-@jwt_required
-@auth_required
-@swag_from("update_user.yml", methods=["PUT"])
-def update_user_view(user_id):
-    dto = UpdateUserRequest(
-        user_id=user_id, **request.form.to_dict(), files=request.files.getlist("files")
-    ).validate_request_and_make_dto()
-    if not dto:
-        return failure_response(
-            UseCaseFailureOutput(type=FailureType.INVALID_REQUEST_ERROR)
-        )
-
-    return UserPresenter().transform(UpdateUserUseCase().execute(dto=dto))
+# fox에서 user 관련 처리하기 때문에 api 주석처리함
+# @api.route("/user/v1/users/<int:user_id>", methods=["PUT"])
+# @jwt_required
+# @auth_required
+# @swag_from("update_user.yml", methods=["PUT"])
+# def update_user_view(user_id):
+#     dto = UpdateUserRequest(
+#         user_id=user_id, **request.form.to_dict(), files=request.files.getlist("files")
+#     ).validate_request_and_make_dto()
+#     if not dto:
+#         return failure_response(
+#             UseCaseFailureOutput(type=FailureType.INVALID_REQUEST_ERROR)
+#         )
+#
+#     return UserPresenter().transform(UpdateUserUseCase().execute(dto=dto))
