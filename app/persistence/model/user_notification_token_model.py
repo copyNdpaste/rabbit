e65b541793b0cd70a773d8e0de635fa24dd579d3
noah.Nam <noah85.Nam@gmail.com>
2021-03-03 10:54:32 +0900

    [Add] user_notification_tokens table 마이그레이트

---
+++ app/persistence/model/user_notification_token_model.py
@@ -0,0 +1,25 @@
+from sqlalchemy import (
+    Column,
+    BigInteger,
+    Integer,
+    String,
+    ForeignKey,
+)
+from sqlalchemy.orm import relationship, backref
+
+from app import db
+from app.persistence.model.user_model import UserModel
+
+
+class UserModel(db.Model):
+    __tablename__ = "user_notification_tokens"
+
+    id = Column(BigInteger().with_variant(Integer, "sqlite"), primary_key=True)
+    user_id = Column(
+        BigInteger().with_variant(Integer, "sqlite"),
+        ForeignKey(UserModel.id),
+        nullable=False,
+    )
+    token = Column(String(50), nullable=True)
+
+    user = relationship("UserModel", backref="user_notification_tokens")
